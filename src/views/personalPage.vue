ac9b88971828ccf2109829041a9fd0bede8b429c
memorXie <x1844591761@163.com>
2020-06-21 14:40:43 +0800

    改动了personalPage.

--- src/views/personalPage.vue
+++ src/views/personalPage.vue
@@ -1,63 +1,71 @@
 <template>
   <div>
     <app-bar></app-bar>
-    <v-card
-            class="mx-auto"
-            max-width="1000"
-    >
+    <v-card class="mx-auto" max-width="1000">
       <v-img
-              class="white--text align-end"
-              height="200px"
-              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        class="white--text align-end"
+        height="200px"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
       >
         <v-card-title>Top 10 Australian beaches</v-card-title>
       </v-img>
       <v-list-item three-line>
-          <v-list-item-content>
-            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+        <v-list-item-content>
+          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
 
-            <v-card-text class="text--primary">
-              <div>Whitehaven Beach</div>
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
 
-              <div>Whitsunday Island, Whitsunday Islands</div>
-            </v-card-text>
-          </v-list-item-content>
-          <v-list-item-avatar
-                  tile
-                  size="80"
-                  color="blue"
-          ></v-list-item-avatar>
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+        </v-list-item-content>
+        <v-list-item-avatar tile size="80" color="blue"></v-list-item-avatar>
       </v-list-item>
 
-
       <v-card-actions>
-          <v-btn
-                  color="orange"
-                  text
-          >
-            Share
-          </v-btn>
+        <v-btn color="orange" text>Share</v-btn>
 
-          <v-btn
-                  color="orange"
-                  text
-          >
-            Explore
-          </v-btn>
+        <v-btn color="orange" text>Explore</v-btn>
       </v-card-actions>
     </v-card>
+    <v-container class="mx-auto py-0">
+      <v-card>
+        <v-tabs v-model="tab" background-color="blue" dark show-arrows centered>
+          <v-tab v-for="item in items" :key="item.tab">{{ item.tab }}</v-tab>
+        </v-tabs>
 
-
-
+        <v-tabs-items v-model="tab">
+          <v-tab-item v-for="item in items" :key="item.tab">
+            <v-card flat>
+              <v-card-text>{{ item.content }}</v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
 <script>
-    import AppBar from "../components/AppBar";
-    export default {
-        components: {AppBar},
-        data () {
-
-        },
-    }
+import AppBar from "../components/AppBar";
+export default {
+  components: { AppBar },
+  data() {
+    return {
+      tab: null,
+      items: [
+        { tab: "One", content: "Tab 1 Content" },
+        { tab: "Two", content: "Tab 2 Content" },
+        { tab: "Three", content: "Tab 3 Content" },
+        { tab: "Four", content: "Tab 4 Content" },
+        { tab: "Five", content: "Tab 5 Content" },
+        { tab: "Six", content: "Tab 6 Content" },
+        { tab: "Seven", content: "Tab 7 Content" },
+        { tab: "Eight", content: "Tab 8 Content" },
+        { tab: "Nine", content: "Tab 9 Content" },
+        { tab: "Ten", content: "Tab 10 Content" }
+      ]
+    };
+  }
+};
 </script>
