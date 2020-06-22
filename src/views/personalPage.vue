a031fb814d923b6d9d38bf285a14c57acf0a1b25
14010007517 <2090124923@qq.com>
2020-06-22 09:30:27 +0800

    conflct merge

--- src/views/personalPage.vue
+++ src/views/personalPage.vue
@@ -48,13 +48,6 @@
             v-on:input="changeToPage()"></v-pagination>
           </v-tab-item>
         </v-tabs-items>
-          <div class="text-center">
-            <v-pagination
-            v-model="page"
-            :length="6"
-            v-on:input="next1(page)"
-          ></v-pagination>
-  </div>
       </v-card>
     </v-card>
     
